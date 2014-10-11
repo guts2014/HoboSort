dd793b974ed0f43e48782c585931eddbd48ecc72
adikus <andrej.hoos@gmail.com>
2014-10-11 12:12:20 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -15,15 +15,19 @@ function generateCustomer(level, employees)
 
 $(document).ready(function () 
 {
-    game.scene = sjs.Scene({w:640, h:480,parent:$("#canvas-container")[0]});
-    game.layer = game.scene.Layer("Front",{useCanvas:true});
-    game.ticker = game.scene.Ticker(draw);
+    $container = $("#canvas-container")
+    game.size = {width: $container.innerWidth(), height: $container.innerWidth()*3/4};
+    game.scene = sjs.Scene({w: game.size.width, h: game.size.height, parent: $container[0]});
+    game.layer = game.scene.Layer("front");
     game.input = game.scene.Input();
+    game.ticker = game.scene.Ticker(draw);
+
     game.ticker.run();
 
     var enemy = game.scene.Sprite('img/employee1.png', game.layer);
-    enemy.move(200, 200);
-    enemy.size(50,50);
+    enemy.move(game.size.width / 2 - 150, game.size.height / 2 - 150);
+    enemy.size(300, 300);
+    enemy.scale(0.25);
     enemy.update();
 });
 
